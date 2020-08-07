74fcdbe1aadbedc839c2b2871ba0e6e5d441c155
Rakmo33 <omkardabir2013@gmail.com>
2020-08-07 09:35:10 +0530

    msg-sending via form

---
+++ Task-5/script.js
@@ -0,0 +1,38 @@
+var inputBox = document.querySelector(".msg-form input");
+var inputButtons = document.querySelectorAll(".msg-form .other-btns");
+var inputBar = document.querySelector(".input-bar");
+var inputForm = document.querySelector(".msg-form");
+
+inputBox.oninput = function(){
+    if(inputBox.value){
+       document.getElementById("camera-btn").setAttribute("src","SVG/smile-solid.svg");
+       inputButtons.forEach(function(btn){
+           btn.style.display = "none";
+       });
+       inputBar.style.gridTemplateColumns = "50px 1fr 55px";
+       document.querySelector(".send-btn").style.display = "block"
+    }
+
+    else{
+        document.getElementById("camera-btn").setAttribute("src","SVG/camera-solid.svg");
+        inputBar.style.gridTemplateColumns = "50px 1fr 45px 45px 45px";
+        inputButtons.forEach(function(btn){
+            btn.style.display = "flex";            
+        });
+        document.querySelector(".send-btn").style.display = "none";
+    }
+}
+
+var sendBtn = document.querySelector(".send-btn");
+
+inputForm.addEventListener("submit", function (e){
+    e.preventDefault();
+
+    document.querySelector(".right .right-ul").innerHTML += 
+    `<li><p>` + inputBox.value + `</p></li>`
+
+    var last = document.querySelector("main");
+    last.scrollTop = last.scrollHeight;
+
+    inputBox.value = "";
+});
